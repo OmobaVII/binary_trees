339c238c63d8dc344f0186e23b844125d7613abe
OmobaVII <omoba.career@gmail.com>
2023-05-30 03:21:05 -0700

    Lowest Common Ancestor

--- 100-binary_trees_ancestor.c
+++ 100-binary_trees_ancestor.c
@@ -7,7 +7,8 @@
  * @second: the second node
  * Return: the common lowest common ancestor or NULL is no common ancestor
  */
-binary_tree_t *binary_trees_ancestor(const binary_tree_t *first, const binary_tree_t *second)
+binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
+				     const binary_tree_t *second)
 {
 	binary_tree_t *parent_first, *parent_second, *lca;
 
@@ -18,12 +19,14 @@ binary_tree_t *binary_trees_ancestor(const binary_tree_t *first, const binary_tr
 	parent_first = first->parent;
 	parent_second = second->parent;
 
-	if (first == parent_second || parent_first == NULL || (parent_first->parent == NULL && parent_second != NULL))
+	if (first == parent_second || parent_first == NULL ||
+	   (parent_first->parent == NULL && parent_second != NULL))
 	{
 		lca = binary_trees_ancestor(first, parent_second);
 		return (lca);
 	}
-	if (second == parent_first || parent_second == NULL || (parent_second->parent == NULL && parent_first != NULL))
+	if (second == parent_first || parent_second == NULL ||
+	   (parent_second->parent == NULL && parent_first != NULL))
 	{
 		lca = binary_trees_ancestor(second, parent_first);
 		return (lca);
