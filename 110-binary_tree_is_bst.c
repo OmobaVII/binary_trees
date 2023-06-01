75f7adbad60f700f4e9ef0671e25a4f41610f18c
OmobaVII <omoba.career@gmail.com>
2023-05-31 23:06:57 -0700

    Is BST

--- 110-binary_tree_is_bst.c
+++ 110-binary_tree_is_bst.c
@@ -4,6 +4,8 @@
 /**
  * is_bst_helper - a function that helps to check if tree is a valid BST
  * @tree: the root node of the tree to check
+ * @min: the minimum number in the tree
+ * @max: the maximum number in the tree
  * Return: 1 for true and 0 for false
  */
 int is_bst_helper(const binary_tree_t *tree, int min, int max)
@@ -23,5 +25,7 @@ int is_bst_helper(const binary_tree_t *tree, int min, int max)
  */
 int binary_tree_is_bst(const binary_tree_t *tree)
 {
+	if (tree == NULL)
+		return (0);
 	return (is_bst_helper(tree, INT_MIN, INT_MAX));
 }
